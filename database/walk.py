b9509d5951da8826dec5191cc3eabe2cdb60d730
Nick Popov <48173606+nickspopov@users.noreply.github.com>
2023-08-28 12:10:13 +0300

    fix crash

--- database/walk.py
+++ database/walk.py
@@ -62,7 +62,13 @@ class Walk(Model):
         return (self.finishedAt - self.startedAt).total_seconds()
 
     def get_avg_speed(self) -> float:
-        return self.get_distance() / self.get_duration()
+        duration = self.get_duration()
+        if duration == 0:
+            return 0.0
+        return self.get_distance() / duration
 
     def get_avg_pace(self) -> float:
-        return self.get_duration() / self.get_distance()
+        distance = self.get_distance()
+        if distance == 0:
+            return 0.0
+        return self.get_duration() / distance
