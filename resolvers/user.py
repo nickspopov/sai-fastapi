5282f89d14e0e6e783686fde5a3eb61b4f17f322
Nick Popov <48173606+nickspopov@users.noreply.github.com>
2023-09-12 17:32:34 +0300

    fix push token duplicates

--- resolvers/user.py
+++ resolvers/user.py
@@ -11,6 +11,13 @@ async def me_resolver(self, info: Info) -> UserType:
 
 async def set_push_token(self, info: Info, token: str) -> UserType:
     user = await check_authentication(info)
+    
+    if not user.pushTokens:
+        user.pushTokens = []
+    
+    if token in user.pushTokens:
+        return user.to_graphQL()
+    
     user.pushTokens.append(token)
     await engine.save(user)
     return user.to_graphQL()
